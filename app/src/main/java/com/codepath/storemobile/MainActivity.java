2cd2ebcdd6118416027f6067cf795e1680459c6c
pvastie <vastiepaulo@gmail.com>
2019-09-16 01:00:36 -0500

    configure parseApp

--- app/src/main/java/com/codepath/storemobile/MainActivity.java
+++ app/src/main/java/com/codepath/storemobile/MainActivity.java
@@ -1,10 +1,16 @@
 package com.codepath.storemobile;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private Button btnOpenStore;
 
 
     @Override
@@ -15,5 +21,17 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle( "Bienvenue" );
         actionBar.getTitle().toString();
+
+        btnLogin = findViewById( R.id.btnLogin );
+        btnOpenStore = findViewById( R.id.btnOpenStoreWelcome );
+
+        btnLogin.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent( MainActivity.this, LoginActivity.class );
+                startActivity( i );
+            }
+        } );
+
     }
 }
--- app/src/main/java/models/Order.java
+++ app/src/main/java/models/Order.java
@@ -3,7 +3,6 @@ package models;
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
 
-
-@ParseClassName( "Product" )
-public class Items extends ParseObject {
+@ParseClassName( "Order" )
+public class Order extends ParseObject {
 }
