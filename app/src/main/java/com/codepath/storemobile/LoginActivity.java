ca1d761eec46289ca2aa3f7bd19cb5b070a7f683
pvastie <vastiepaulo@gmail.com>
2019-09-16 19:11:02 -0500

    SignUp/Login

--- app/src/main/java/com/codepath/storemobile/LoginActivity.java
+++ app/src/main/java/com/codepath/storemobile/LoginActivity.java
@@ -3,12 +3,21 @@ package com.codepath.storemobile;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoginActivity";
+
     private Button btnLoginUser, btnSignUpUser, btnMyStoreUserLogin;
+    private EditText etUsernameLogin, etPasswordLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +28,18 @@ public class LoginActivity extends AppCompatActivity {
         btnSignUpUser = findViewById( R.id.btn_sign_up_user );
         btnMyStoreUserLogin = findViewById( R.id.btn_my_store_user_login );
 
+        etUsernameLogin = findViewById( R.id.et_username_login );
+        etPasswordLogin = findViewById( R.id.et_password_login );
+
         btnLoginUser.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent loginIntent = new Intent( LoginActivity.this, StoreActivity.class );
-                startActivity( loginIntent );
+
+                String username1 = etUsernameLogin.getText().toString();
+                String password1 = etPasswordLogin.getText().toString();
+
+                login( username1, password1 );
+
             }
         } );
 
@@ -42,5 +58,30 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity( myStoreIntent );
             }
         } );
+    }
+
+
+    private void login(String username, String password) {
+        ParseUser.logInInBackground( username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null){
+                    // TODO: Better error handling
+                    Log.e(TAG, "Issues with login");
+                    e.printStackTrace();
+                    return;
+                }
+                // TODO: navigate to new activity if user login property
+
+                goStoreActivity();
+
+            }
+        } );
+    }
+
+    private void goStoreActivity() {
+        Intent loginIntent = new Intent( LoginActivity.this, StoreActivity.class );
+        startActivity( loginIntent );
+        finish();
     }
 }
--- app/src/main/java/com/codepath/storemobile/ParseApplication.java
+++ app/src/main/java/com/codepath/storemobile/ParseApplication.java
@@ -8,6 +8,8 @@ import com.parse.ParseObject;
 import models.Items;
 import models.Order;
 import models.Store;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 public class ParseApplication extends Application {
 
@@ -19,13 +21,19 @@ public class ParseApplication extends Application {
         ParseObject.registerSubclass( Items.class);
         ParseObject.registerSubclass( Store.class);
 
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
         // set applicationId, and server server based on the values in the Heroku settings.
         // clientKey is not needed unless explicitly configured
         // any network interceptors must be added with the Configuration Builder given this syntax
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("storemob") // should correspond to APP_ID env variable
                 .clientKey("IdappStoreMobilexxYYvvs578Workgroup")  // set explicitly unless clientKey is explicitly configured on Parse server
-                //.clientBuilder(builder)
+                .clientBuilder(builder)
                 .server("https:storemob.herokuapp.com/parse").build());
     }
 }
